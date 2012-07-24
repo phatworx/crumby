3824d7cd85f525c9e790fdb01360df742cdec55c
Marco Scholl <develop@marco-scholl.de>
2012-07-24 22:43:07 +0200

    cleanup and 2 negative spec for first? and last?

--- spec/crumby/breadcrumb_spec.rb
+++ spec/crumby/breadcrumb_spec.rb
@@ -11,14 +11,12 @@ describe Crumby::Breadcrumb do
 
     subject { Crumby::Breadcrumb.new(breadcrumbs, 1, breadcrumb_label, breadcrumb_route, breadcrumb_options) }
 
-
     its(:breadcrumbs) { should equal breadcrumbs }
     its(:position) { should eq 1 }
     its(:label) { should eq breadcrumb_label }
     its(:route) { should eq breadcrumb_route }
     its(:options) { should eq breadcrumb_options }
   end
-
 
   context "on breadcrumbs with 10 breadcrumb" do
     let(:breadcrumbs) { breadcrumbs = stub :breadcrumbs, count: 10 }
@@ -31,15 +29,15 @@ describe Crumby::Breadcrumb do
     context "first breadcrumb" do
       subject { Crumby::Breadcrumb.new breadcrumbs, 0, "Test" }
       its(:first?) { should be_true }
+      its(:last?) { should_not be_true }
     end
 
     context "last breadcrumb" do
       subject { Crumby::Breadcrumb.new breadcrumbs, 9, "Test" }
+      its(:first?) { should_not be_true }
       its(:last?) { should be_true }
     end
 
   end
-
-
 
 end

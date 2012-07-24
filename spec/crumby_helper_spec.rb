d3f766015a2426df3ccd37a51c97d4994d423920
Marco Scholl <develop@marco-scholl.de>
2012-07-24 13:00:51 +0200

    optimize typo

--- spec/crumby_helper_spec.rb
+++ spec/crumby_helper_spec.rb
@@ -6,7 +6,7 @@ end
 
 class DummyModelName
   def human
-    "sample string"
+    "dummy human name"
   end
 end
 
@@ -29,24 +29,24 @@ describe Crumby::Helper do
     context "with one argument" do
       subject { controller.add_crumb(first_argument) }
 
-      context "first is a string" do
+      context "that is a string" do
         let(:first_argument) { "example string" }
         its(:label) { should equal first_argument }
         its(:route) { should be_nil }
       end
 
-      context "first is a symbol" do
+      context "that is a symbol" do
         let(:first_argument) { :example_string }
         its(:label) { should eq first_argument.to_s.humanize }
         its(:route) { should equal first_argument }
       end
 
-      context "first is an object with model_name method" do
+      context "that is an object with model_name method" do
         let(:first_argument) { DummyModel.new }
         its(:label) { should eq first_argument.model_name.human }
         its(:route) { should equal first_argument }
       end
-      context "first is an array of objects" do
+      context "that is an array of objects" do
         context "last is an object with model_name method" do
           let(:first_argument) { [:admin, DummyModel.new] }
           its(:label) { should eq first_argument.last.model_name.human }

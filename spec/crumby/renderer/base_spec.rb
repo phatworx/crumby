9133e24136925a9dd4a860b7c103f33886afd23d
Marco Scholl <develop@marco-scholl.de>
2012-07-25 15:55:12 +0200

    add spec for Crumby::Renderer::Base

---
+++ spec/crumby/renderer/base_spec.rb
@@ -0,0 +1,32 @@
+# encoding: utf-8
+require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
+
+describe Crumby::Renderer::Base do
+  let(:trail) { stub :trail }
+  subject { Crumby::Renderer::Base.new(trail) }
+
+  describe "#new" do
+    its(:trail) { should equal trail }
+  end
+
+  describe "#render" do
+    pending "todo"
+  end
+
+  describe "#default_options" do
+    it { subject.default_options.should be_empty }
+  end
+
+  describe "#render_list" do
+    it "should raise a NotImplementedError error" do
+      expect { subject.render_list(stub) }.to raise_error NotImplementedError
+    end
+  end
+
+  describe "#render_entry" do
+    it "should raise a NotImplementedError error" do
+      expect { subject.render_entry(stub, stub) }.to raise_error NotImplementedError
+    end
+  end
+
+end

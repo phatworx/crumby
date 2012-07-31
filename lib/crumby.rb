a20d518d8c584030fd6a142095bef1c1e9070668
Marco Scholl <develop@marco-scholl.de>
2012-07-31 08:09:06 +0200

    add confgure support move lazy load into an init

--- lib/crumby.rb
+++ lib/crumby.rb
@@ -1,26 +1,38 @@
+require 'active_support/lazy_load_hooks'
+require 'active_support/core_ext/module/attribute_accessors'
+
 module Crumby
   autoload :Entry, 'crumby/entry'
   autoload :Trail, 'crumby/trail'
   autoload :Helper, 'crumby/helper'
   autoload :Renderer, 'crumby/renderer'
 
-
   # default renderer
   mattr_accessor :renderer
   @@renderer = Renderer::Haml
 
   class << self
-    def setup
-      yield self
+
+    # configure crumby
+    #   Crumby.configure do
+    #     # default renderer
+    #     # renderer = Renderer::Haml
+    #   end
+    def configure(&block)
+      self.instance_eval &block
+    end
+
+    def init!
+      ActiveSupport.on_load(:action_controller) do
+        ActionController::Base.send :include, Crumby::Helper::ControllerHelper
+      end
+
+      ActiveSupport.on_load(:action_view) do
+        ActionView::Base.send :include, Crumby::Helper::ViewHelper
+      end
     end
   end
 
 end
 
-ActiveSupport.on_load(:action_controller) do
-  ActionController::Base.send :include, Crumby::Helper::ControllerHelper
-end
-
-ActiveSupport.on_load(:action_view) do
-  ActionView::Base.send :include, Crumby::Helper::ViewHelper
-end
+Crumby.init!
